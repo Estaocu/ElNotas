82eb6b0f941fd3d769f7d9f4dae26cefc93fac56
Estaocu <moranbmaguilar@gmail.com>
2026-09-18 17:24:33 +0200

    gitflow-feature-stash: MeterRework

--- Assets/Scripts/PlayerHP.cs
+++ Assets/Scripts/PlayerHP.cs
@@ -5,89 +5,18 @@ using System.Collections;
 
 public class PlayerHP : MonoBehaviour
 {
-    [Header("Settings")]
-    [SerializeField] private int maxHp = 2;
-    [SerializeField] private int stunTime = 4;
-    [SerializeField] private int immunityTime = 8;
-
     // Campos privados (Estado interno)
     private int currentHp;
-    private float currentMeter => meter.CurrentMeter;
-    private bool damageImmune;
-    private bool isStunned;
+    private float currentMeter => meter.currentMeter;
 
     // Propiedades calculadas (Solo lectura para el exterior)
-    public bool isMeterFull => meter.HasFullMeter;
+    public bool isMeterFull => meter.hasFullMeter;
     public int GetCurrentHp() => currentHp;
 
-    [SerializeField] private PlayerRhythmController meter;
-
-    private RhythmClock rhythmClock;
-
-    private void Awake()
-    {
-        currentHp = maxHp;
-        rhythmClock = FindFirstObjectByType<RhythmClock>();
-    }
+    [SerializeField] private Meter meter;
 
     public void OnHit(int dmg)
     {
-        if (damageImmune) return;
-
-        if (isMeterFull)
-        {
-            DoParry();
-        }
-        else
-        {
-            if(currentMeter != 0)
-            {
-                meter.ConsumeFullMeter();
-                StartCoroutine(StunRoutine());
-                return;
-            }
-
-            currentHp = Mathf.Max(currentHp - dmg, 0);
-            meter.ConsumeFullMeter();
-            Debug.Log($"HITS LEFT: {currentHp} / {maxHp}");
-            CheckHp();
-            
-        }
-    }
-    private void DoParry()
-    {
-        meter.ConsumeFullMeter();
-        StartCoroutine(ImmunityRoutine());
-        Debug.Log("PARRIED");
-    }
-
-    private IEnumerator ImmunityRoutine()
-    {
-        damageImmune = true;
-        Debug.Log("Player DMG Immune");
-        yield return rhythmClock.WaitForSubBeats(immunityTime);
-        damageImmune = false;
-        Debug.Log("Player can be damaged");
-    }
-
-    private void CheckHp()
-    {
-        if (currentHp <= 0)
-        {
-            Debug.Log("GAME OVER.");
-        }
-    }
-
-    private IEnumerator StunRoutine()
-    {
-        isStunned = true;
-        Debug.Log("Player Stunned");
-        ActionMapsManager.Instance.SwapActionMap("RestrictedInput");
-
-        yield return rhythmClock.WaitForSubBeats(stunTime);
-
-        isStunned = false;
-        ActionMapsManager.Instance.SwapActionMap("Gameplay");
-        Debug.Log("Player no longer Stunned");
+        Debug.Log("hit XD");
     }
 }
--- Assets/Scripts/Singing/Singer.cs
+++ Assets/Scripts/Singing/Singer.cs
@@ -22,8 +22,9 @@ public class Singer : MonoBehaviour
 {
     private RhythmTimingValidator timingValidator;
     private MelodyDatabase database;
-    private PlayerRhythmController player;
+    private PlayerRhythmController instrument;
     private SingerVoice voice;
+    private Meter meter;
 
     [Header("Settings")]
     [SerializeField] private Transform soundwaveSpawnpoint;
@@ -34,7 +35,7 @@ public class Singer : MonoBehaviour
     private readonly notesEnum[] noteBuffer = new notesEnum[4];
     private int notesPlayed;
 
-    public bool IsPlayer => player != null;
+    public bool IsPlayer => instrument != null;
 
     public event Action<Melody> onSoundwaveSpawned;
 
@@ -47,8 +48,7 @@ public class Singer : MonoBehaviour
             timingValidator = GameManager.Instance.RhythmTimingValidator;
         }
 
-        player =
-            GetComponent<PlayerRhythmController>();
+        instrument = GetComponent<PlayerRhythmController>();
 
         voice = GetComponent<SingerVoice>();
 
@@ -60,17 +60,17 @@ public class Singer : MonoBehaviour
 
     private void OnEnable()
     {
-        if (player != null)
+        if (instrument != null)
         {
-            player.OnNoteAccepted += OnPlayerNoteAccepted;
+            instrument.OnNoteAccepted += OnPlayerNoteAccepted;
         }
     }
 
     private void OnDisable()
     {
-        if (player != null)
+        if (instrument != null)
         {
-            player.OnNoteAccepted -= OnPlayerNoteAccepted;
+            instrument.OnNoteAccepted -= OnPlayerNoteAccepted;
         }
     }
 
@@ -94,7 +94,7 @@ public class Singer : MonoBehaviour
     private void CheckPlayerMelodies()
     {
         IReadOnlyList<PlayerRhythmController.PlayedNote> notes =
-            player.CurrentNotes;
+            instrument.CurrentNotes;
 
         if (notes.Count < 4)
         {
@@ -148,31 +148,26 @@ public class Singer : MonoBehaviour
     private void TrySpawnPlayerMelody(Melody melody)
     {
 
-        Debug.Log($"Melody inputed and launched. | Meter %: {player.CheckMeter()}");
+        Debug.Log($"Melody inputed and launched. | instrument %: {meter.currentMeter}");
 
-        switch (player.CheckMeter())
+
+        if(meter.currentMeter <= 8)
         {
-            case > 50:
-            player.ConsumeHalfMeter();
-            break;
-
-
-            case < 50:
-            Debug.LogWarning("Tried to play melody with less than half meter.");
+            Debug.LogWarning("Tried to play melody with less than half instrument.");
             return;
-            
         }
-
+        
+        meter.ConsumeFullMeter();
         lastSingTime = Time.time;
 
         NoteSystem.EmitMelody(melody, soundwaveSpawnpoint.position, this);
 
         onSoundwaveSpawned?.Invoke(melody);
 
-        player.ClearMelody();
+        instrument.ClearMelody();
     }
 
-    // Used by NPCs and other non-player singers.
+    // Used by NPCs and other non-instrument singers.
     public void AddNote(notesEnum note)
     {
         if (IsPlayer)
